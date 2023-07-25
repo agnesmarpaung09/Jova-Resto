6087dbe831572f1e917e26a9be56ab3a513cd16c
agnesmarpaung09 <agnesmarpaung03@gmail.com>
2023-07-25 16:41:46 +0700

    commit

--- app/footer.tsx
+++ app/footer.tsx
@@ -2,50 +2,43 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-sky-600 py-4 text-gray-200">
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center px-4 lg:px-40">
-          <div className="pr-4">
-            <img src="/images/LOGO.png" alt="Logo" className="w-13 h-6" />
-          </div>
-          <nav>
-            <ul className="flex space-x-5">
-              <li>
-                <a href="/home" className="hover:underline hover:text-white">
-                  Beranda
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/features"
-                  className="hover:underline hover:text-white"
-                >
-                  Features
-                </a>
-              </li>
-              <li>
-                <a href="/price" className="hover:underline hover:text-white">
-                  Harga
-                </a>
-              </li>
-              <li>
-                <a href="/FAQ" className="hover:underline hover:text-white">
-                  FAQ
-                </a>
-              </li>
-              <li>
-                <a href="/contact" className="hover:underline hover:text-white">
-                  Hubungi Kami
-                </a>
-              </li>
-            </ul>
-          </nav>
+    <footer className="bg-cyan-600 py-4 text-white">
+      <nav className="flex items-center justify-between px-4 mx-auto max-w-7xl">
+        <div>
+          <img
+            src="/images/LOGO2.png"
+            className="h-10 mr-6 font-bold text-3xl"
+            alt="Logo"
+          />
         </div>
-        <hr className="border-t-2 border-white my-1" />
-        <div className="text-xs text-center">&copy; copyright by jovaresto</div>
-      </div>
+        <div style={{ marginBottom: "0px" }}>
+          <NavLink href="/beranda" className="text-white">
+            Beranda
+          </NavLink>{" "}
+          <NavLink href="/features" className="text-white">
+            Fitur
+          </NavLink>{" "}
+          <NavLink href="/harga" className="text-white">
+            Harga
+          </NavLink>{" "}
+          <NavLink href="/faq" className="text-white">
+            FAQ
+          </NavLink>{" "}
+          <NavLink href="/hubungi-kami" className="text-white">
+            Hubungi Kami
+          </NavLink>
+        </div>
+      </nav>
+      <hr style={{ borderTop: "1px solid white", margin: "16px 0" }} />
+      <p className="text-center text-sm">&copy; Copyright by JovaResto</p>
     </footer>
   );
 };
+
+const NavLink = ({ href, children, className }) => (
+  <a href={href} className={`text-black hover:text-blue-200 mx-2 ${className}`}>
+    {children}
+  </a>
+);
 
 export default Footer;
