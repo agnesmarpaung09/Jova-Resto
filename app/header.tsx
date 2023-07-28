8383f19cc7ea8042d97bd790a128e4f630fcf679
Sintiasilaen <sintiasilaen67@gmail.com>
2023-07-28 22:37:36 +0700

    update

--- app/header.tsx
+++ app/header.tsx
@@ -1,44 +1,31 @@
 import React from "react";
-import { FaChevronDown } from "react-icons/fa"; // Impor ikon panah dari Heroicons
-
+import { FaChevronDown } from "react-icons/fa";
 const Header = () => {
   return (
-    <header className="relative flex items-center container ml-16 px-5 sm:px-7 py-3 lg:px-40">
-      <div className="flex items-center">
-        <div className="logo mr-8">
+    <header className=" relative flex items-center container mx-auto px-5 sm:px-7 py-3 lg:px-40">
+      <div className="flex items-center w-full">
+        <div className="logo mr-4">
           <img src="images/LOGO.png" alt="Logo" className="w-40 h-8" />
         </div>
         <nav className="hidden lg:flex">
-          <ul className="flex space-x-6 justify-left">
-            <li className="mb-2 md:mb-0">
-              <NavLink
-                href="/beranda"
-                className="text-lg font-semibold text-black"
-              >
+          <ul className="flex items-center">
+            <li className="mr-2 md:mb-0">
+              <NavLink href="/beranda" className="text-lg  text-black">
                 Beranda
               </NavLink>{" "}
             </li>
-            <li className="mb-2 md:mb-0">
-              <NavLink
-                href="/features"
-                className="text-lg font-semibold text-black"
-              >
+            <li className="mr-2 md:mb-0">
+              <NavLink href="/features" className="text-lg  text-black">
                 Fitur
               </NavLink>{" "}
             </li>
-            <li className="mb-2 md:mb-0">
-              <NavLink
-                href="/harga"
-                className="text-lg font-semibold text-black"
-              >
+            <li className="mr-2 md:mb-0">
+              <NavLink href="/harga" className="text-lg  text-black">
                 Harga
               </NavLink>{" "}
             </li>
-            <li className="mb-2 md:mb-0">
-              <NavLink
-                href="/harga"
-                className="text-lg font-semibold text-black"
-              >
+            <li className="mr-2 md:mb-0">
+              <NavLink href="/harga" className="text-lg  text-black">
                 Solusi Bisnis{" "}
                 <span className="group inline-block">
                   <FaChevronDown
@@ -48,16 +35,13 @@ const Header = () => {
                 </span>
               </NavLink>{" "}
             </li>
-            <li className="mb-2 md:mb-0">
-              <NavLink href="/faq" className="text-lg font-semibold text-black">
+            <li className="mr-2 md:mb-0">
+              <NavLink href="/faq" className="text-lg text-black">
                 FAQ
               </NavLink>{" "}
             </li>
-            <li className="mb-2 md:mb-0">
-              <NavLink
-                href="/hubungi-kami"
-                className="text-lg font-semibold text-black"
-              >
+            <li className="mr-2 md:mb-0">
+              <NavLink href="/hubungi-kami" className="text-lg  text-black">
                 Hubungi Kami
               </NavLink>
             </li>
@@ -66,18 +50,16 @@ const Header = () => {
 
         <nav className="hidden lg:flex ml-auto">
           <ul className="flex items-center">
-            <li className="ml-4">
-              <button className="text-black text-lg font-semibold underline">
-                Log In
-              </button>
+            <li className="mr-4">
+              <button className="text-black text-lg underline">Log In</button>
             </li>
-            <li className="ml-4">
-              <button className="bg-purple-800 text-white px-5 py-2 rounded-full mr-4 hover:bg-purple-700 text-md">
+            <li className="mr-2">
+              <button className="bg-purple-900 text-white px-5 py-2 rounded-full hover:bg-purple-700 text-lg">
                 Coba Gratis
               </button>
             </li>
-            <li className="ml-4">
-              <select className="bg-white ml-4 py-1 text-md">
+            <li className="mr-2">
+              <select className="bg-white ml-4 py-1 text-lg">
                 <option value="id">ID</option>
                 <option value="en">EN</option>
               </select>
